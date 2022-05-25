8a320b2124b4446a1cd8e27ad6b16ae8da205634
patrick <official.denkyipatrick@gmail.com>
2022-05-25 17:15:58 +0000

    Update phone number

--- src/app/services/utility.service.ts
+++ src/app/services/utility.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UtilityService {
   bankName: String;
@@ -15,21 +15,25 @@ export class UtilityService {
   euroSymbol: string;
   dollarSymbol: string;
 
-  constructor(private http: HttpClient, 
-    private constantsService: ConstantsService) {
+  constructor(
+    private http: HttpClient,
+    private constantsService: ConstantsService
+  ) {
     this.selectedCurrency = JSON.parse(localStorage.getItem('currency'));
 
     this.euroSymbol = '€';
     this.dollarSymbol = '$';
 
-    this.shortBankName = "Lollands";
-    this.bankPhoneNumber = "+31 970 0503 4293";
+    this.shortBankName = 'Lollands';
+    this.bankPhoneNumber = '+45 5492 1134';
     this.bankName = `${this.shortBankName} Credit Union`;
     this.applicationName = `${this.bankName} - Online Banking Platform`;
   }
 
   public getAccountTypes(): Observable<object[]> {
-    return this.http.get<object[]>(`${this.constantsService.ACCOUNT_TYPES_URL}`);
+    return this.http.get<object[]>(
+      `${this.constantsService.ACCOUNT_TYPES_URL}`
+    );
   }
 
   public createUser(data: object): Observable<object> {
@@ -37,50 +41,76 @@ export class UtilityService {
   }
 
   public signin(data: object): Observable<object> {
-    return this.http.post<object>(`${this.constantsService.USERS_URL}/auth`, data);
+    return this.http.post<object>(
+      `${this.constantsService.USERS_URL}/auth`,
+      data
+    );
   }
 
   public getUserAccounts(userId: string) {
-    return this.http.get<object[]>(`${this.constantsService.USERS_URL}/${userId}/accounts`);
+    return this.http.get<object[]>(
+      `${this.constantsService.USERS_URL}/${userId}/accounts`
+    );
   }
 
   public getUserAccount(userAccountId: string) {
-    return this.http.get<object>(`${this.constantsService.USER_ACCOUNTS_URL}/${userAccountId}`)
+    return this.http.get<object>(
+      `${this.constantsService.USER_ACCOUNTS_URL}/${userAccountId}`
+    );
   }
 
   public getAccountTransfers(userAccountId: string) {
-    return this.http.get<object[]>(`${this.constantsService.USER_ACCOUNTS_URL}/${userAccountId}/transfers`);
+    return this.http.get<object[]>(
+      `${this.constantsService.USER_ACCOUNTS_URL}/${userAccountId}/transfers`
+    );
   }
 
   public getUserAccountDeposits(userAccountId: string) {
-    return this.http.get<object[]>(`${this.constantsService.USER_ACCOUNTS_URL}/${userAccountId}/deposits`);
+    return this.http.get<object[]>(
+      `${this.constantsService.USER_ACCOUNTS_URL}/${userAccountId}/deposits`
+    );
   }
-  
+
   public getUserAccountWithdrawals(userAccountId: string) {
-    return this.http.get<object[]>(`${this.constantsService.USER_ACCOUNTS_URL}/${userAccountId}/withdrawals`);
+    return this.http.get<object[]>(
+      `${this.constantsService.USER_ACCOUNTS_URL}/${userAccountId}/withdrawals`
+    );
   }
-  
+
   public getCurrencies() {
     return this.http.get<any[]>(`${this.constantsService.CURRENCIES_URL}`);
   }
-  
+
   public getCurrency(currencyId: string) {
-    return this.http.get<any>(`${this.constantsService.CURRENCIES_URL}/${currencyId}`);
+    return this.http.get<any>(
+      `${this.constantsService.CURRENCIES_URL}/${currencyId}`
+    );
   }
 
   public changeCurrencyRate(currencyId: string, rate: number) {
-    return this.http.put<any>(`${this.constantsService.CURRENCIES_URL}/${currencyId}`, { rate: rate })
+    return this.http.put<any>(
+      `${this.constantsService.CURRENCIES_URL}/${currencyId}`,
+      { rate: rate }
+    );
   }
 
   searchTransferReceivers(accountIdOrNumber: string) {
-    return this.http.get<object[]>(`${this.constantsService.USER_ACCOUNTS_URL}/search?q=${accountIdOrNumber}`)
+    return this.http.get<object[]>(
+      `${this.constantsService.USER_ACCOUNTS_URL}/search?q=${accountIdOrNumber}`
+    );
   }
 
   public transferMoney(data: object) {
-    return this.http.post<object>(`${this.constantsService.MONEY_TRANSFERS_URL}`, data);
+    return this.http.post<object>(
+      `${this.constantsService.MONEY_TRANSFERS_URL}`,
+      data
+    );
   }
 
   public sendContactData(data: object) {
-    return this.http.post<object>(`${this.constantsService.ADMIN_CONTACT_URL}`, data);
+    return this.http.post<object>(
+      `${this.constantsService.ADMIN_CONTACT_URL}`,
+      data
+    );
   }
 }
