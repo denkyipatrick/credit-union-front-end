068f9f64f0ab29349671b5150f16b0053ac4f7ed
patrick <official.denkyipatrick@gmail.com>
2022-05-29 12:05:31 +0000

    Fix: correct sentence by removing 'your'.

--- src/app/banking/withdrawals/withdrawals.component.ts
+++ src/app/banking/withdrawals/withdrawals.component.ts
@@ -5,28 +5,27 @@ import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-withdrawals',
   templateUrl: './withdrawals.component.html',
-  styleUrls: ['./withdrawals.component.scss']
+  styleUrls: ['./withdrawals.component.scss'],
 })
 export class WithdrawalsComponent implements OnInit {
-  @Input() withdrawals: any[]
+  @Input() withdrawals: any[];
   @Input() withdrawnAmount: number;
-  tableColumns: string[] = ['accountNumber', 'amount', 'date']
+  tableColumns: string[] = ['accountNumber', 'amount', 'date'];
 
   constructor(private dialogOpener: MatDialog) {
     this.withdrawnAmount = 0;
   }
 
-  ngOnInit(): void {
-  }
-  
+  ngOnInit(): void {}
+
   makeWithdrawal() {
     this.dialogOpener.open(OkDialogComponent, {
       data: {
         title: 'Error! Visit Our Branches',
-        message: 'You cannot make an online withdrawal due to security concerns. ' + 
-        'You can make a withdrawal by using your our ATM machines or visiting any of our branches.'
-      }
+        message:
+          'You cannot make an online withdrawal due to security concerns. ' +
+          'You can make a withdrawal by using our ATM machines or visiting any of our branches.',
+      },
     });
   }
-
 }
